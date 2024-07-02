6be8b4da6c272e9da187f7083244c3725c881e65
Jia Chen <grievejia@fb.com>
2024-07-02 10:25:39 -0700

    Remove sexplib and stdio as build time dependencies

--- lib/taglessFinal/misc/fix_python_config.ml
+++ lib/taglessFinal/misc/fix_python_config.ml
@@ -1,6 +1,17 @@
+let read_all filename =
+  let channel = open_in filename in
+  let data = really_input_string channel (in_channel_length channel) in
+  close_in channel;
+  data
+
+let write_all ~name content =
+  let channel = open_out name in
+  output_string channel content;
+  close_out_noerr channel
+
 let () =
-  let input_content = Stdio.In_channel.read_all Sys.argv.(1) in
+  let input_content = read_all Sys.argv.(1) in
   let output_content =
     Str.global_replace (Str.regexp {|\$(\([A-Za-z0-9]*\))|}) "${\\1}" input_content
   in
-  Stdio.Out_channel.write_all "python-config-fixed.sh" ~data:output_content
+  write_all ~name:"python-config-fixed.sh" output_content
